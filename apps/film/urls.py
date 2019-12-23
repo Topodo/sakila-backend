8216ca8643af2ab520094fe0dc0b755472174b9e
Ariel <ariel.tirado@usach.cl>
2019-12-23 02:52:39 -0300

    added CORS

--- apps/film/urls.py
+++ apps/film/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from apps.film.views import ListFilms, DetailFilm, ActorsOfTheFilm, BindActorToFilm
 
 urlpatterns = [
-    path('', ListFilms.as_view()),
+    path('films/', ListFilms.as_view()),
     path('films/<int:pk>/', DetailFilm.as_view()),
     path('films/<int:pk>/actors/', ActorsOfTheFilm.as_view()),
     path('films/<int:film_id>/actors/<int:actor_id>/', BindActorToFilm.as_view())
